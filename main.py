e17c9a54a70bc6c54f3b5fd65094ab14d83d1cc6
XP20 <67698795+XP20@users.noreply.github.com>
2023-03-25 02:22:53 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from pywinauto import Application
+from ctypes import c_int, Structure, POINTER, windll, byref, sizeof, cast, pointer
+
+class ACCENT_POLICY(Structure):
+    _fields_ = [("AccentState", c_int),
+                ("AccentFlags", c_int),
+                ("GradientColor", c_int),
+                ("AnimationId", c_int)]
+
+# Get the window handle
+hwnd = Application().connect(path="alacritty.exe").top_window().handle
+
+accent_policy = ACCENT_POLICY()
+accent_policy.AccentState = 3 # ACCENT_ENABLE_BLURBEHIND value is 3
+
+class WINDOWCOMPOSITIONATTRIB(Structure):
+    _fields_ = [("Attribute", c_int),
+                ("Data", POINTER(ACCENT_POLICY)),
+                ("SizeOfData", c_int)]
+
+wca = WINDOWCOMPOSITIONATTRIB()
+wca.Attribute = 19 # WCA_ACCENT_POLICY value is 19
+wca.Data = POINTER(ACCENT_POLICY)()
+wca.SizeOfData = sizeof(accent_policy)
+
+# Get the pointer to the ACCENT_POLICY instance
+p_accent_policy = cast(pointer(accent_policy), POINTER(c_int))
+wca.Data = cast(p_accent_policy, POINTER(ACCENT_POLICY))
+
+SetWindowCompositionAttribute = windll.user32.SetWindowCompositionAttribute
+SetWindowCompositionAttribute(hwnd, byref(wca))
