fdbf48739b7711a0ed4ea1e7d08e0118346652dc
XP20 <67698795+XP20@users.noreply.github.com>
2023-04-03 14:49:42 +0300

    Update for win11 support

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from pywinauto import Application
 from ctypes import c_int, Structure, POINTER, windll, byref, sizeof, cast, pointer
+import sys
+
+def IsWin11():
+    if sys.getwindowsversion().build > 20000: return True
+    else: return False
 
 class ACCENT_POLICY(Structure):
     _fields_ = [("AccentState", c_int),
                 ("AccentFlags", c_int),
                 ("GradientColor", c_int),
                 ("AnimationId", c_int)]
-
+ 
 # Get the window handle
 hwnd = Application().connect(path="alacritty.exe").top_window().handle
 
 accent_policy = ACCENT_POLICY()
 accent_policy.AccentState = 3 # ACCENT_ENABLE_BLURBEHIND value is 3
+
+if IsWin11():
+    accent_policy.AccentFlags = 0x20 | 0x40 # Set the ACCENT_ENABLE_BLURBEHIND and ACCENT_INVALID_OPACITY flag
 
 class WINDOWCOMPOSITIONATTRIB(Structure):
     _fields_ = [("Attribute", c_int),
